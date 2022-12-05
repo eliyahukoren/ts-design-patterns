152e65fb82506581be989473f2bf6fb0a4b10998
eliyahukoren <elik.koren@gmail.com>
2022-12-05 15:51:35 +0200

    just simple class

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,39 @@
-console.log('Hi there');
+// Set
+
+let numberSet: Set<number>;
+numberSet = new Set([1, 2, 3, 4]);
+
+const stringSet: Set<string> = new Set(["a", "b", "c"]);
+
+class Cat {
+	constructor() {}
+
+	walk(): void {
+		console.log("cat is walking");
+	}
+}
+
+const cat = new Cat();
+cat.walk();
+
+interface IAnimal {
+	name: string;
+	age: number;
+	feed(food: string, amount: number):void;
+}
+
+class Dog implements IAnimal {
+	constructor(public name: string, public age: number) {}
+
+	feed(food: string, amount: number): void {
+		console.log(`${this.name} have a ${amount} kg of ${food} `)
+	}
+
+	walk(): void {
+		console.log(`${this.name} can walk`);
+	}
+}
+
+const dog = new Dog("Lola", 7);
+dog.feed('beef', 0.78);
+dog.walk();
